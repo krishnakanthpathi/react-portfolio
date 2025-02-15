54ca77de25ab6f21002b3d08a1ecda6205ce96f9
krishnakanth <krishnakanthpathi@gmail.com>
2025-02-15 22:39:13 +0530

    adjusted app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-import "../src/assets/css/style.css"
-import { useState , useEffect } from "react"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Skills from "./components/Skills"
-import Projects from "./components/Projects"
-import Achievements from "./components/Achievements"
-import Footer from "./components/Footer"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../src/assets/css/style.css";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Achievements from "./components/Achievements";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+    const [darkMode, setDarkMode] = useState(true);
 
-    const [ Dark , setDark ] = useState(false) 
+    const toggleDarkMode = () => {
+        setDarkMode((prevMode) => !prevMode);
+    };
 
-    const toggleDark = () => {
-        setDark(!Dark)
-    }
-    useEffect(() =>{
-        if(Dark){
-          console.log('dark')
-            document.body.classList.add('bg-dark')
-            document.body.classList.add('text-light')
-        }else{
-          console.log('light')
-            document.body.classList.remove('text-light')
-            document.body.classList.remove('bg-dark') 
-        }
-    } , [Dark])
-    
+    useEffect(() => {
+        document.body.classList.toggle("bg-dark", darkMode);
+        document.body.classList.toggle("text-light", darkMode);
+        console.log(darkMode ? "Dark mode activated" : "Light mode activated");
+    }, [darkMode]);
+
     return (
-    <>
-      <Navbar darkmode={toggleDark} theme = {Dark} />
-      <Hero theme = {Dark}/>
-      <About theme = {Dark} />
-      <Skills theme = {Dark} />
-      <Projects theme = {Dark} />
-      <Achievements theme = {Dark} />  
-      <Footer  />
-      
-    </>
-    )
-  
+        <>
+            <Navbar darkmode={toggleDarkMode} theme={darkMode} />
+            <Hero theme={darkMode} />
+            <About theme={darkMode} />
+            <Skills theme={darkMode} />
+            <Projects theme={darkMode} />
+            <Achievements theme={darkMode} />
+            <Footer />
+        </>
+    );
 }
-export default App
+
+export default App;
